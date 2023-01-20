a00f4a8d7387eb9280f405f41993c8f564f31742
Adebozz <ademolaadeigbe3@gmail.com>
2023-01-20 22:32:23 +0100

    chatbot

--- hello/CHATBOT/app.py
+++ hello/CHATBOT/app.py
@@ -3,3 +3,9 @@ import json
 import pickle
 import numpy as np
 
+import nltk
+from nltk.stem import WordNetLemmatizer
+
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Activition, Dropout
+from tensorflow.keras.optimizers import SGD
