a50b26a1648e460e87006318913e7c0d698512ab
Adebozz <ademolaadeigbe3@gmail.com>
2023-01-23 22:32:55 +0100

    chatbot update

--- hello/CHATBOT/app.py
+++ hello/CHATBOT/app.py
@@ -30,4 +30,39 @@ for intent in intents['intents']:
 words = [lemmatizer.lemmatize(words) for word in words if words not in ignore_letters]
 words = sorted(set(words))
 
-print(words)
+
+pickle.dump(words, open('words.pk1','wb'))
+pickle.dump(words, open('classes.pk', 'wb'))
+
+training = []
+output_empty = [0] * len(classes)
+
+for document in documents:
+    bag =[]
+    word_patterns = document[]
+    word_patterns = [lemmatizer.lemmatize(word.lower() for word in word_patterns)]
+    for word in words:
+        bag.append(1) if word in word_patterns else bag.append(0)
+
+    output_row = list(output_empty)
+    output_row[classes.index(document[1])] = 1
+    training.append([bag, output_row])
+random.shuffle(training)
+training = np.array(training)
+
+train_x = list(training[:,0])
+train_y = list(training[:, 1])
+
+model = Sequential()
+model.add(Dense(128, input_shape=(len(train_x[0]), activation = 'relu')))
+model.add(Dropout(0,5))
+model.add(Dense(64, activation = 'relu'))
+model.add(Dropout(0,5))
+model.add(Dense(len(train_y[0]), activation = 'softmax'))
+
+sgd = SGD(lr = 0.01, decay=1e-6, momentum=0.9, nesterov=True)
+model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
+
+model.fit(np.array(train_x), np.array(train_y), epochs=200, batch_size=5, verbose=1)
+model.save('chatbot_model.model')
+print("Done")
